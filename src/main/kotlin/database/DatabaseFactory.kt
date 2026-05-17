b939e2b747ef32323cde57a577363fa30969cfd1
Rikuto <dialogroth@gmail.com>
2026-05-18 03:17:44 +0900

    fix database env for render

--- src/main/kotlin/database/DatabaseFactory.kt
+++ src/main/kotlin/database/DatabaseFactory.kt
@@ -14,22 +14,38 @@ import org.jetbrains.exposed.sql.transactions.transaction
 object DatabaseFactory {
 
     fun init(application: Application) {
-        val config = application.environment.config
-        val url = config.property("database.url").getString()
-        val driver = config.property("database.driver").getString()
-        val user = config.property("database.user").getString()
-        val password = config.property("database.password").getString()
-        val maxPoolSize = config.property("database.maxPoolSize").getString().toInt()
+
+        val url = System.getenv("DATABASE_URL")
+            ?: application.environment.config.propertyOrNull("database.url")?.getString()
+            ?: error("DATABASE_URL is not set")
+
+        val driver = System.getenv("DATABASE_DRIVER")
+            ?: "org.postgresql.Driver"
+
+        val user = System.getenv("DATABASE_USER")
+            ?: ""
+
+        val password = System.getenv("DATABASE_PASSWORD")
+            ?: ""
+
+        val maxPoolSize = System.getenv("DATABASE_POOL_SIZE")
+            ?.toIntOrNull()
+            ?: 10
 
         val hikariConfig = HikariConfig().apply {
-            jdbcUrl = url
+            jdbcUrl = if (url.startsWith("postgres://")) {
+                url.replace("postgres://", "jdbc:postgresql://")
+            } else {
+                url
+            }
+
             driverClassName = driver
             username = user
             this.password = password
+
             maximumPoolSize = maxPoolSize
             isAutoCommit = false
             transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-            validate()
         }
 
         Database.connect(HikariDataSource(hikariConfig))
